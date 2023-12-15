22f06656cffe6fa6eccf84ffc0bf61a4df13cd26
PratikMbade <pratikmbade18@gmail.com>
2023-12-16 01:05:26 +0530

    added-utils

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
-import { parse } from "dotenv";
+
 import express from "express";
-import todosData from '../todos.json' assert { type: 'json' };
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 
 
 const app = express();
 
 
+app.uss(cors({
+    origin:process.env.CORS_ORIGIN,
+    creadentials:true
+}))
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export {app}
